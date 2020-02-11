3da5504733e0565006ca0e269e3f8a17e3fc64a0
Eshan Wadhwa <50282448+eshanw48@users.noreply.github.com>
2020-02-10 22:11:39 -0500

    Song class is complete

--- src/SongApp/Song.java
+++ src/SongApp/Song.java
@@ -1,26 +1,56 @@
-
+	
 //class for properties of a song
 
 public class Song{
 
-	//every song needs a name and artist, so that will by my constructor
+//every song needs a name and artist, so that will by my constructor
+	private String title;
+	private String artist;
+	private String album;
+	private String year;
 	
-	String name;
-	String artist;
-	String album;
-	int year;
-
-
-	public Song(String name, String artist){
-		this.name=name;
-		this.artist=artist;
+	public Song(String title, String artist, String album, String year)
+	{
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	
+	}
+//Below are our getter methods
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getArtist() {
+		return this.artist;
 	}
 
+	public String getAlbum() {
+		return this.album;
+	}
+
+	public String getYear() {
+		return this.year;
+	}
+//Below are our setter methods
 	
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
 
+	public void setAlbum(String album) {
+		this.album = album;
+	}
 
-
-
-
+	public void setYear(String year) {
+		this.year = year;
+	}
 }
+
+
