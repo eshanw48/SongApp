fd1b1fd92d85f3d0bb4eaeeda496b8c99089cae3
vishal929 <48373882+vishal929@users.noreply.github.com>
2020-02-18 02:25:24 -0500

    Add files via upload
    Added lots of functionality for user safety. Included packages.

---
+++ SongLib/src/SongApp/SongApp.java
@@ -0,0 +1,46 @@
+/*By Vishal Patel and Eshan Wadhwa */
+package SongApp;
+
+import SongView.SongController;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+
+
+public class SongApp extends Application{
+
+	
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		FXMLLoader loader= new FXMLLoader();
+		loader.setLocation(getClass().getResource("/SongView/SongUIView.fxml"));
+			
+		//AnchorPane for root is a resizable Node
+		AnchorPane root = (AnchorPane) loader.load();
+
+		//getting controller
+		SongController songController = loader.getController();
+		songController.start(primaryStage);
+		Scene window = new Scene(root);
+
+		primaryStage.setScene(window);
+		primaryStage.show();
+	}	
+
+	public static void main(String[] args){
+		launch(args);
+	}
+
+	 
+	
+
+
+
+}
